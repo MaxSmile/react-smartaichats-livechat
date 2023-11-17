ed96d3c11497f5a23a1fd1ec18a0591f5f8481a5
Maxim Vasilkov <dev@smrt16.com>
2023-11-17 15:44:10 +0200

    avoid double append

--- dist/SmartAiChatsLiveChat.js
+++ dist/SmartAiChatsLiveChat.js
@@ -10,13 +10,18 @@ function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return 
 function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && Object.prototype.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
 var SmartAiChatsLiveChat = function SmartAiChatsLiveChat(_ref) {
   var chatId = _ref.chatId;
+  var base_url = 'https://smrtaichats.com';
   (0, _react.useEffect)(function () {
     // Function to load the script
     var loadScript = function loadScript() {
-      var script = document.createElement('script');
-      script.src = "https://smrtaichats.com/chat/".concat(chatId);
-      script.async = true;
-      document.body.appendChild(script);
+      // Check if the script is already added
+      if (!document.getElementById("smartaichats-script")) {
+        var script = document.createElement('script');
+        script.src = "".concat(base_url, "/chat/").concat(chatId);
+        script.async = true;
+        script.id = "smartaichats-script";
+        document.body.appendChild(script);
+      }
     };
 
     // Load the script when the component mounts
